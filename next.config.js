229453f1543ce5f671374bf7630f9abe78613932
irie612 <57913132+irie612@users.noreply.github.com>
2023-12-06 23:35:45 +0000

    Update nextjs config for deploying to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+    basePath: process.env.NODE_ENV === "production" ? "/jake_website": undefined,
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    reactStrictMode: true,
+}
 
 module.exports = nextConfig
