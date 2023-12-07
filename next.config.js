ad729011d5898c4754d29c75c6c10bca80f97352
irie612 <57913132+irie612@users.noreply.github.com>
2023-12-07 00:35:50 +0000

    Remove experimental appDir option in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
     output: "export",
     basePath: process.env.NODE_ENV === "production" ? "/jake_website": undefined,
-    experimental: {
-        appDir: true,
-    },
     images: {
         unoptimized: true,
     },
