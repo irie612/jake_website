2b7dfaffde000b346dbc19e9bf91e765c52ee216
irie612 <57913132+irie612@users.noreply.github.com>
2024-02-16 13:43:24 +0000

    add link to header to go to home page

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Image from "next/image";
 import { header_img } from "@/components/image_loader";
+import Link from "next/link";
 
 const Header = () => {
 	return (
 		<div className="top-0 flex w-full justify-center">
-			<Image
-				src={header_img}
-				alt="Header Image"
-				className="lg:max-h-72 md:max-h-40 sm:max-h-32 px-2 md:px-4 aspect-[539/170] w-auto"
-				priority={true}
-			/>
+			<Link href="/">
+				<Image
+					src={header_img}
+					alt="Header Image"
+					className="lg:max-h-72 md:max-h-40 sm:max-h-32 px-2 md:px-4 aspect-[539/170] w-auto"
+					priority={true}
+				/>
+			</Link>
 		</div>
 	);
 };
