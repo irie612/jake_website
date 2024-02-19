0564778de92fc812b75aa27766610e8f5fed3b83
irie612 <57913132+irie612@users.noreply.github.com>
2024-02-19 02:30:04 +0000

    Fix errors in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,14 +6,13 @@ const Footer = () => {
         <footer className="bg-ivory dark:bg-maire text-maire dark:text-ivory py-4 flex justify-center">
             <div className="flex flex-col text-xs lg:w-8/12 md:w-9/12 sm:w-10/12 w-11/12">
                 <p className='text-sm'>
-                    <Link href="/" className="nav underline">Tattoo</Link> | <Link href="/flash" className="nav underline">Flash</Link> | <a href="https://jakecordal.bigcartel.com" className="nav underline">Shop</a>
+                    <Link href="/" className="nav underline">Tattoos</Link> | <Link href="/flash" className="nav underline">Flash</Link> | <a href="https://jakecordal.bigcartel.com" className="nav underline">Shop</a>
                 </p>
                 <br />
                 <div className='flex justify-between flex-col sm:flex-row'>
-                    <p>Copytright &copy; {new Date().getFullYear()} Jake Cordal Tattoo</p>
+                    <p>Copyright &copy; {new Date().getFullYear()} Jake Cordal Tattoo</p>
                     <p>Website by <a href='https://irie612.github.io' className='nav underline'>Irie Railton</a></p>
                 </div>
-                
             </div>
         </footer>
     );
