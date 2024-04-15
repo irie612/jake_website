1c6b8419f82185d470394b80ff7a7df7a892997f
irie612 <57913132+irie612@users.noreply.github.com>
2024-04-15 11:21:05 +0100

    add transition to hover color change, tidy up some stuff, reduce styling repetition

--- components/floating_button.tsx
+++ components/floating_button.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { IconType } from "react-icons";
-import { FaArrowUp, FaInstagram, FaPhone } from "react-icons/fa";
+import { FaArrowUp } from "react-icons/fa";
 
 interface FloatingButtonProps {
 	position: "top-left" | "top-right" | "bottom-left" | "bottom-right";
@@ -35,7 +35,7 @@ const FloatingButton: React.FC<FloatingButtonProps> = ({
 	return (
 		<button
 			onClick={onClick}
-			className={`fixed z-10 p-2 rounded-full dark:bg-ivory bg-maire shadow-lg hover:bg-gold active:bg-gold ${className} ${getPositionStyles()}`}
+			className={`fixed z-10 p-2 rounded-full dark:bg-ivory bg-maire shadow-lg hover:bg-gold active:bg-gold transition-color duration-400 ease-ease ${className} ${getPositionStyles()}`}
 		>
 			<Icon className="w-8 h-8 text-ivory dark:text-maire" />
 		</button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,47 +1,56 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  theme: {
-    colors: {
-      'ivory': 'rgb(243,243,239)',
-      'maire': 'rgb(27,27,20)',
-      'gold': 'rgb(228, 209, 0)',
-    },
-    extend: {
-      keyframes: {
-        'slide-in-bottom': {
-          '0%': {
-            transform: 'translateY(200%)',
-            visibility: 'visible',
-          },
-          '100%': {
-            transform: 'translateY(0)',
-          },
-        },
-        'slide-out-bottom': {
-          '0%': {
-            transform: 'translateY(0)',
-          },
-          '100%': {
-            visibility: 'hidden',
-            transform: 'translateY(200%)',
-          },
-        },
-      },
-      animation: {
-        'slide-in-bottom': 'slide-in-bottom 0.5s ease-out',
-        'slide-out-bottom': 'slide-out-bottom 0.5s ease-in forwards',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	theme: {
+		colors: {
+			ivory: "rgb(243,243,239)",
+			maire: "rgb(27,27,20)",
+			gold: "rgb(228, 209, 0)",
+		},
+		extend: {
+			transitionProperty: {
+				color: "color",
+			},
+			transitionDuration: {
+				"400": "400ms",
+			},
+			transitionTimingFunction: {
+				ease: "ease",
+			},
+			keyframes: {
+				"slide-in-bottom": {
+					"0%": {
+						transform: "translateY(200%)",
+						visibility: "visible",
+					},
+					"100%": {
+						transform: "translateY(0)",
+					},
+				},
+				"slide-out-bottom": {
+					"0%": {
+						transform: "translateY(0)",
+					},
+					"100%": {
+						visibility: "hidden",
+						transform: "translateY(200%)",
+					},
+				},
+			},
+			animation: {
+				"slide-in-bottom": "slide-in-bottom 0.5s ease-out",
+				"slide-out-bottom": "slide-out-bottom 0.5s ease-in forwards",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
