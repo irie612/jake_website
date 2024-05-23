853214c231c6f870fe33bff80a99b6e327ea239e
irie612 <57913132+irie612@users.noreply.github.com>
2024-05-23 11:25:35 +0100

    update next.js and react-icons. Added sharp for image loading

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: process.env.NODE_ENV === "production" ? "/jake_website" : undefined,
-    images: {
-        unoptimized: true,
-    },
-    reactStrictMode: true,
-}
+	output: "export",
+	basePath:
+		process.env.NODE_ENV === "production" ? "/jake_website" : undefined,
+	images: {
+		unoptimized: true,
+	},
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
