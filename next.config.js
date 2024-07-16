3b00a5b28de235168cc768cb1a40cbeb077ac58e
Irie Railton <irierailton@gmail.com>
2024-07-17 00:05:55 +0100

    add auto deployment to hostinger

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
-	// basePath:
-	// 	process.env.NODE_ENV === "production" ? "/jake_website" : undefined,
 	images: {
 		unoptimized: true,
 	},
