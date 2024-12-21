3191b9776f05cd25199456076525979df00a6053
IrieR <57913132+irie612@users.noreply.github.com>
2024-12-21 12:07:55 +0000

    add spans to footer to fix spacing

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,9 +6,9 @@ const Footer = () => {
         <footer className="bg-ivory dark:bg-maire text-maire dark:text-ivory py-4 flex justify-center" data-nosnippet>
             <div className="flex flex-col text-xs lg:w-8/12 md:w-9/12 sm:w-10/12 w-11/12">
                 <p className='text-sm'>
-                    <Link href="/" className="nav underline">Tattoos</Link> |
-                    <Link href="/flash" className="nav underline">Flash</Link> | 
-                    <a href="https://shop.jakecordaltattoo.co.uk" className="nav underline">Shop</a> | 
+                    <Link href="/" className="nav underline">Tattoos</Link> | <span/>
+                    <Link href="/flash" className="nav underline">Flash</Link> | <span/>
+                    <a href="https://shop.jakecordaltattoo.co.uk" className="nav underline">Shop</a> | <span/>
                     <Link href="/book" className='nav underline'>Book</Link>
                 </p>
                 <br />
